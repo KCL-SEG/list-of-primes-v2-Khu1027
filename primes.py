6ae9a0a08437b7b1e629dad79160816bf7d50bd9
Khulood <khulood1027@gmail.com>
2023-10-01 21:26:38 +0100

    Finish exercise 1.4

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("The number must be a positive number over 0")
     list = []
+    num = 2
+    while len(list) != number_of_primes:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            list.append(num)
+        num += 1
     return list
+
+print(primes(7))
+print(primes(0))
+print(primes(-15))
+
